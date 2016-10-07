92097eb0e8abf4a5ebabe9c8b9ed673e1c8af0ff
tosih <sohail@runnable.com>
2016-10-07 12:45:04 -0700

    Strip down app, make basic Hello World.

--- src/Index.js
+++ src/Index.js
@@ -2,14 +2,11 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import App from './components/App'
-import About from './components/About'
 
 window.React = React
 
 render(
   (<Router history={hashHistory}>
-    <Route path='/' component={App}>
-      <Route path='/about' component={About} />
-    </Route>
+    <Route path='/' component={App} />
   </Router>), document.getElementById('content')
 )
--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router'
 
 const App = ({ children }) => (
   <div>
-    <header>
-      <Link to='/'> Home
-      </Link>
-      <Link to='/about'> About
-      </Link>
-    </header>
     <section className='splash'>
       {children || <div className='content'>
         <h2>Hello World!</h2>
+        <p>
+          Runnable is a sandbox service that creates <b>full-stack environments</b> for your GitHub branches, so your team can integrate and test more often.
+        </p>
       </div>}
     </section>
   </div>
